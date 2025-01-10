d1183f2951f09b73f5813d7393cf2956b4909ab5
hongfetti <ryan.hong1994@gmail.com>
2025-01-10 17:18:58 -0500

    fixed typo in pathing to script.js file and created dog array

---
+++ assets/js/results.js
@@ -0,0 +1,64 @@
+const availDogs = [dog1, dog2, dog3, dog4, dog5, dog6]
+
+const dog1 = {
+    name: Murphy,
+    age: puppy,
+    size: small,
+    gender: male,
+    img: src="./images/Designer.png",
+}
+const dog2 = {
+    name: Lulu,
+    age: adult,
+    size: medium,
+    gender: female,
+    img: src="./images/Designer.png",
+}
+const dog3 = {
+    name: Buster,
+    age: senior,
+    size: large,
+    gender: male,
+    img: src="./images/Designer.png",
+}
+const dog4 =  {
+    name: Ava,
+    age: puppy,
+    size: small,
+    gender: female,
+    img: src="./images/Designer.png",
+}
+const dog5 =  {
+    name: Theo,
+    age: adult,
+    size: medium,
+    gender: male,
+    img: src="./images/Designer.png",
+}
+const dog6 =  {
+    name: Marley,
+    age: senior,
+    size: large,
+    gender: female,
+    img: src="./images/Designer.png",
+}
+
+const displayResults = function () {
+    let searchCriteria = readLocalStorage()
+    // compare properties of searchCriteria and objects in availDogs for matches
+
+    // pull objects off array that match criteria and create new array
+    
+    // display cards containing object information for new array
+        // create element      
+        // populate information on element
+        // append element to page
+}
+
+// displayResults() 
+
+let test = function () {
+    console.log (availDogs)
+}
+
+test ()
