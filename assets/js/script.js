e3bfe948d2f48a119a7704c5249af7a6e88640af
je210506 <je210506@ucf.edu>
2025-01-10 18:48:52 -0500

    scrapping my functions temporarily

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
-const subBtn = document.querySelector('#submitbtn')
-const formEl = document.querySelector('#homeform')
-const errorMessage = "Please complete the form.";
-const errorEl = document.querySelector('errorMessage');
-formEl.appendChild(errorEl);
+// const subBtn = document.querySelector('#submitbtn')
+// const formEl = document.querySelector('#homeform')
+// const errorMessage = "Please complete the form.";
+// const errorEl = document.querySelector('errorMessage');
+// formEl.appendChild(errorEl);
 
-const handleFormSubmit = function(event) {
-    event.preventDefault();
-    const selectionEl = document.querySelector('.class-select')
+// const handleFormSubmit = function(event) {
+//     event.preventDefault();
+//     const selectionEl = document.querySelector('.class-select')
 
-    if(!selectionEl) {
-        document.querySelector('error')
-    }
-};
+//     if(!selectionEl) {
+//         document.querySelector('error')
+//     }
+// };
 
-const storeLocalStorage = function() {
-    const formSelect = document.querySelectorAll('.form-select');
-    const formData = {
+// const storeLocalStorage = function() {
+//     const formSelect = document.querySelectorAll('.form-select');
+//     const formData = {
 
-    }
-}
+//     }
+// }
