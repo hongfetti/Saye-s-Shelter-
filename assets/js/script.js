7b81d6df021a7027f7df817e9f75c4abd97f99a1
je210506 <je210506@ucf.edu>
2025-01-10 18:34:55 -0500

    i worked on functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 const subBtn = document.querySelector('#submitbtn')
+const formEl = document.querySelector('#homeform')
+const errorMessage = "Please complete the form.";
+const errorEl = document.querySelector('errorMessage');
+formEl.appendChild(errorEl);
 
-subBtn.addEventListener('submit', function(event) {
-    event.preventDefault()
-    location.assign('./results.html')
-})
+const handleFormSubmit = function(event) {
+    event.preventDefault();
+    const selectionEl = document.querySelector('.class-select')
+
+    if(!selectionEl) {
+        document.querySelector('error')
+    }
+};
+
+const storeLocalStorage = function() {
+    const formSelect = document.querySelectorAll('.form-select');
+    const formData = {
+
+    }
+}
