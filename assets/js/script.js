acd52ddef3456f6387a058ae53f8c9b3301657ee
je210506 <je210506@ucf.edu>
2025-01-12 13:51:40 -0500

    first page redirects to results page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,26 @@
-const subBtn = document.querySelector('#submitbtn')
-const formEl = document.querySelector('#homeform')
-const errorMessage = "Please complete the form.";
-const selectionEl = document.querySelectorAll('.form-select')
-// declare the variables of the form choice by size, age, gender
-let error = document.querySelector('#error');
-
-
-const handleFormSubmit = function(event) {
+//prevent form from submitting the traditional way
+document.getElementById('homeform').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    if(selectionEl === '') {
-        error.textcontent = errorMessage
-    } else {
-        let searchCriteria = {
-            
-        }
-    }
-};
+//values from the dropdown
+const pet = document.getElementById('pet').value;
+const age = document.getElementById('age').value;
+const gender = document.getElementById('gender').value;
+const size = document.getElementById('size').value;
 
-let formData = {
-
+//check and make sure the dropdowns don't have the "option choices" selected
+if (pet === ''  || age === '' || gender === '' || size === '') {
+    document.getElementById('error').textContent = 'Please select an option from all the dropdowns to submit.';
+    return;
 }
 
-
-const readLocalStorage = function () {
-    let searchCriteria = JSON.parse(localStorage.getItem(searchCriteria))
-    return searchCriteria
+//if they select "no" for adopt a pet, the page doesn't submit
+const want = document.getElementById('pet').value;
+if (pet === '2') {
+    document.getElementById('error').textContent = 'Change response to "Yes" if you want to adopt.';
+    return
 }
 
-const storeLocalStorage = function (data) {
-    localStorage.setItem('searchCriteria', JSON.stringify(data))
-}
-git
+//redirect to next page 
+window.location.href = './results.html'
+});
