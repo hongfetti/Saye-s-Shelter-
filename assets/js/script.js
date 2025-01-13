104ec7ada3832ca8102cbb9e6ea40fc00c328789
hongfetti <ryan.hong1994@gmail.com>
2025-01-13 13:11:18 -0500

    changes to cards saved to dynamically form

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,16 @@
 //     window.location.href = './results.html'
 // })
 //values from the dropdown
-const pet = document.getElementById('pet').value;
-const age = document.getElementById('age').value;
-const gender = document.getElementById('gender').value;
-const size = document.getElementById('size').value;
+const pet = document.getElementById('pet');
+const age = document.getElementById('age');
+const gender = document.getElementById('gender');
+const size = document.getElementById('size');
 
-const searchValues = {
-    age: age.value,
-    size: size.value,
-    gender: gender.value,
-}
+// const searchValues = {
+//     age: age.value,
+//     size: size.value,
+//     gender: gender.value,
+// }
 
 const readLocalStorage = function () {
     return JSON.parse(localStorage.getItem('searchCriteria'))
@@ -23,37 +23,46 @@ const storeLocalStorage = function (value) {
     localStorage.setItem('searchCriteria', JSON.stringify(value))
 }
 
-// const redirectPage = function () {
-//     window.location.href = './results.html'
+// // const redirectPage = function () {
+// //     window.location.href = './results.html'
+// // }
+
+// const formSubmit = function (event) {
+//     event.preventDefault()
+//     if (pet === '2') {
+//         //if they select "no" for adopt a pet, the page doesn't submit
+//         document.getElementById('error').textContent = 'Change response to "Yes" if you want to adopt.';
+//         return
+//         //check and make sure the dropdowns don't have the "option choices" selected
+//     } else if (pet === '' || age === '' || gender === '' || size === '') {
+//         document.getElementById('error').textContent = 'Please select an option from all the dropdowns to submit.';
+//         return
+//     } else {
+//         let searchInput = []
+//         const searchValues = {
+//             age: age.value,
+//             size: size.value,
+//             gender: gender.value,
+//         };
+//         searchCriteria = searchInput.push(searchValues)
+//         storeLocalStorage(searchCriteria)
+
+//         redirectPage('/results.html')
+//     }
 // }
 
-const formSubmit = function (event) {
-    event.preventDefault()
-    if (pet === '2') {
-        //if they select "no" for adopt a pet, the page doesn't submit
-        document.getElementById('error').textContent = 'Change response to "Yes" if you want to adopt.';
-        //check and make sure the dropdowns don't have the "option choices" selected
-    } else if (pet === '' || age === '' || gender === '' || size === '') {
-        document.getElementById('error').textContent = 'Please select an option from all the dropdowns to submit.';
-    } else {
-        let searchInput = []
-        const searchValues = {
-            age: age.value,
-            size: size.value,
-            gender: gender.value,
-        };
-        searchCriteria = searchInput.push(searchValues)
-        storeLocalStorage(searchCriteria)
+// document.getElementById('homeform').addEventListener('submit', formSubmit)
 
-        redirectPage('/results.html')
-    }
-}
+// let redirectURL = '';
 
-document.getElementById('homeform').addEventListener('submit', formSubmit)
+// const redirectPage = function (url) {
+//   redirectURL = url;
+//   location.assign(redirectURL);
+//   return
+// };
 
-let redirectURL = '';
-
-const redirectPage = function (url) {
-  redirectURL = url;
-  location.assign(redirectURL);
-};
+localStorage.setItem('searchCriteria', JSON.stringify({
+    age: 'puppy',
+    size: 'large',
+    gender: 'male',
+}))
