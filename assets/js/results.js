324740263ad28c3b09baeef81259493dd1d29e48
hongfetti <ryan.hong1994@gmail.com>
2025-01-13 16:09:28 -0500

    added modal to form and updated dog info

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,45 +1,63 @@
 
 const dog1 = {
-    name: 'Murphy',
+    name: 'Scruffy',
+    ageYrs: 1,
+    breed: 'Maltese',
     age: 'puppy',
     size: 'small',
     gender: 'male',
-    img: "./assets/images/Designer.png",
+    description: 'Scruffy is a sweet, energetic puppy who is looking for a forever home to grow up. He is great with kids and other animals!',
+    img: "./assets/images/Scruffy.jpg",
 }
 const dog2 = {
     name: 'Lulu',
+    ageYrs: 7,
+    breed: 'Corgi',
     age: 'adult',
     size: 'medium',
     gender: 'female',
-    img: "Designer.png",
+    description: 'Lulu is a happy and playful Corgi who would love to go to a home that has kids to play with.',
+    img: 'assets/images/lulu.avif',
 }
 const dog3 = {
-    name: 'Buster',
+    name: 'Max',
+    ageYrs: 9,
+    breed: 'Pitbull',
     age: 'senior',
-    size: 'large',
+    size: 'medium',
     gender: 'male',
-    img: "Designer.png",
+    description: 'Max is a playful Pitbull who loves animals, but is not good with kids',
+    img: 'assets/images/max.jpg',
 }
 const dog4 = {
     name: 'Ava',
+    ageYrs: 1,
+    breed: 'Chihuahua',
     age: 'puppy',
     size: 'small',
     gender: 'female',
-    img: "Designer.png",
+    description: 'Ava is a cuddly puppy who loves to run around in extreme short bursts and then nap it off for a few hours.',
+    img: 'assets/images/ava.webp',
 }
 const dog5 = {
-    name: 'Theo',
-    age: 'adult',
+    name: 'Jack',
+    ageYrs: 2,
+    breed: 'Golden Retreiver',
+    age: 'puppy',
     size: 'medium',
     gender: 'male',
-    img: "Designer.png",
+    description: 'Jack is a loving Golden Retriever who wants to go to a and happy fun home. He loves cats but must be the only dog in the house.',
+    img: 'assets/images/jack.jpg',
 }
 const dog6 = {
-    name: 'Marley',
-    age: 'senior',
+    name: 'Bella',
+    breed: 'Poodle Mix',
+    ageYrs: 4,
+    age: 'adult',
     size: 'large',
     gender: 'female',
-    img: "Designer.png",
+    description: 'Bella is a silly and spoiled Poodle mix who loves kids and animals. She will make everyone happy with her cheerful personality.',
+    img: 'assets/images/bella.jpg',
 }
 const availDogs = [dog1, dog2, dog3, dog4, dog5, dog6]
 const searchCriteria = JSON.parse(localStorage.getItem('formData'))
@@ -76,7 +94,6 @@ const displayResults = function () {
             createCard(dog)
         });
     } else {
-        console.log('hello')
         noResults()
     }
 }
@@ -95,9 +112,9 @@ const createCard = function (dog) {
     createdSection.appendChild(rowDiv)
 
     const dogImg = document.createElement('img')
-    dogImg.setAttribute('src', './assets/images/')
+    dogImg.setAttribute('src', dog.img)
     dogImg.setAttribute('class', 'img-fluid rounded-start')
-    dogImg.setAttribute('alt', 'photo of `${dog.name}`')
+    dogImg.setAttribute('alt', 'photo of ' + dog.name)
     rowDiv.appendChild(dogImg)
 
     const dogCard = document.createElement('section')
@@ -111,7 +128,7 @@ const createCard = function (dog) {
 
     const dogName = document.createElement('h5')
     dogName.setAttribute('class', 'card-title')
-    dogName.textContent = `${dog.name}`
+    dogName.textContent = dog.name
     dogInfoDisplay.appendChild(dogName)
 
     const dogBioShort = document.createElement('p')
@@ -139,16 +156,12 @@ const createCard = function (dog) {
     extraInfo.setAttribute('id', 'collapseExample')
     dogInfoDisplay.appendChild(extraInfo)
 
-    const expandCardTitle = document.createElement('p')
-    expandCardTitle.textContent = 
-    extraInfo.appendChild(expandCardTitle)
-
     const expandCardBody = document.createElement('section')
     expandCardBody.setAttribute('class', 'card-body')
     extraInfo.appendChild(expandCardBody)
 
     const formTitle = document.createElement('h5')
-    formTitle.textContent = 'Inquire about `${dog.name}`'
+    formTitle.textContent = 'Inquire about ' + dog.name
     expandCardBody.appendChild(formTitle)
 
     const inquireForm = document.createElement('form')
@@ -206,8 +219,66 @@ const createCard = function (dog) {
     const formBtn = document.createElement('button')
     formBtn.setAttribute('type', 'submit')
     formBtn.setAttribute('class', 'btn btn-primary')
+    formBtn.setAttribute('data-bs-toggle', 'modal')
+    formBtn.setAttribute('data-bs-target', '#exampleModal')
     formBtn.textContent = 'Submit'
     inquireForm.appendChild(formBtn)
+
+    const inquiryModal = document.createElement('section')
+    inquiryModal.setAttribute('class', 'modal fade')
+    inquiryModal.setAttribute('id', 'exampleModal')
+    inquiryModal.setAttribute('tabindex', '-1')
+    inquiryModal.setAttribute('aria-labelledby', 'exampleModalLabel')
+    inquiryModal.setAttribute('aria-hidden', 'true')
+    inquireForm.appendChild(inquiryModal)
+
+    const modalDialogue = document.createElement('section')
+    modalDialogue.setAttribute('class', 'modal-dialog')
+    inquiryModal.appendChild(modalDialogue)
+
+    const modalContent = document.createElement('section')
+    modalContent.setAttribute('class', 'modal-content')
+    inquiryModal.appendChild(modalContent)
+
+    const modalHeader = document.createElement('header')
+    modalHeader.setAttribute('class', 'modal-header')
+    modalContent.appendChild(modalHeader)
+
+    const modalTitle = document.createElement('h1')
+    modalTitle.setAttribute('class', 'modal-title fs-5')
+    modalTitle.setAttribute('id', 'exampleModalLabel')
+    modalTitle.textContent = 'Inquiry Submission'
+    modalHeader.appendChild(modalTitle)
+
+    const closeBtn = document.createElement('button')
+    closeBtn.setAttribute('type', 'button')
+    closeBtn.setAttribute('class', 'btn-close')
+    closeBtn.setAttribute('data-bs-dismiss', 'modal')
+    closeBtn.setAttribute('aria-label', 'Close')
+    modalHeader.appendChild(closeBtn)
+
+    const modalBody = document.createElement('article')
+    modalBody.setAttribute('class', 'modal-body')
+    modalBody.textContent = 'Congratulations! Your inquiry has been successfully submitted!'
+    modalContent.appendChild(modalBody)
+
+    const modalFooter = document.createElement('section')
+    modalFooter.setAttribute('class', 'modal-footer')
+    modalContent.appendChild(modalFooter)
+
+    const dismissBtn = document.createElement('button')
+    dismissBtn.setAttribute('type', 'button')
+    dismissBtn.setAttribute('class', 'btn btn-secondary')
+    dismissBtn.setAttribute('data-bs-dismiss', 'modal')
+    dismissBtn.textContent = 'Close'
+    modalFooter.appendChild(dismissBtn)
+
+    inquireForm.addEventListener('submit', function (event){
+        event.preventDefault()
+        
+    })
 }
+
+
 
 displayResults() 
